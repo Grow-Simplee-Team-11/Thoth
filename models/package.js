aff3f40610ffebe36440262a1e4820e64177d8d4
rajivharlalka <rajivharlalka009@gmail.com>
2023-01-17 03:21:01 +0530

    remove items model

--- models/package.js
+++ models/package.js
@@ -4,14 +4,20 @@ const Schema = mongoose.Schema;
 const packageSchema = new Schema(
     {
         image_url: {type: String},
-        item_id: {type: Schema.Types.ObjectId, required: true},
+        item_id: {type: String},
         awb_no: {type: String},
-        deliver_to: {name: {type: String, required: true}, phone_number: {type: String, required: true}},
+        deliver_to: {name: {type: String}, phone_number: {type: String}},
         status: {type: String, enum: ["CREATED", "DELIVERED", "PICKED", "FAKE_ATTEMPT", "DAMAGED"], default: "CREATED"},
         coordinates: {
-            latitude: {type: Number, required: true},
-            longitude: {type: Number, required: true},
-            address: {type: String, required: true},
+            latitude: {type: Number},
+            longitude: {type: Number},
+            address: {type: String},
+        },
+        dimensions: {
+            length: {type: String},
+            breadth: {type: String},
+            height: {type: String},
+            weight: {type: String},
         },
         delivered_time: {type: Date},
         type: {type: String, enum: ["DELIVERY", "PICKUP"]},
@@ -28,7 +34,6 @@ export default Package;
 - package_id
 */
 
-
 //TODO: change deliver_to,deliver_time name
 /*
 Wrapper to change format of returned package based on address
@@ -41,5 +46,3 @@ Wrapper to change format of returned package based on address
     }
 }]
 */
-
-
