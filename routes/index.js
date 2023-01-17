c9f151e947b76797b1192775f855cf9e24e01765
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-01-17 21:55:03 +0530

    Register route endpoints

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@
 
 import express from "express";
 import riderRoute from "./rider.route.js";
+import routeRoute from "./route.route.js";
 import packageRoute from "./package.route.js";
 import authRoute from "./auth.route.js";
 import config from "../config/config.js";
@@ -22,6 +23,10 @@ const defaultRoutes = [
     {
         path: "/rider",
         route: riderRoute,
+    },
+    {
+        path: "/route",
+        route: routeRoute,
     },
     {
         path: "/package",
