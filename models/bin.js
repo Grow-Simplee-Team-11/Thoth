62d51662ff81d611062e1ca970d142f7cb4ab45f
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-01-24 19:26:10 +0530

    Update Number attributes in Bin and Package

--- models/bin.js
+++ models/bin.js
@@ -4,21 +4,21 @@ const Schema = mongoose.Schema;
 const binSchema = new Schema(
     {
         dimensions: {
-            length: {type: String, required: true},
-            breadth: {type: String, required: true},
-            height: {type: String, required: true},
-            weight: {type: String, required: true},
+            length: {type: Number, required: true},
+            breadth: {type: Number, required: true},
+            height: {type: Number, required: true},
+            weight: {type: Number, required: true},
         },
         packages: [
             {
                 package_id: {type: Schema.Types.ObjectId},
-                length: {type: String, required: true},
-                breadth: {type: String, required: true},
-                height: {type: String, required: true},
-                x: {type: String, required: true},
-                y: {type: String, required: true},
-                z: {type: String, required: true},
-                weight: {type: String, required: true},
+                length: {type: Number, required: true},
+                breadth: {type: Number, required: true},
+                height: {type: Number, required: true},
+                x: {type: Number, required: true},
+                y: {type: Number, required: true},
+                z: {type: Number, required: true},
+                weight: {type: Number, required: true},
             },
         ],
     },
