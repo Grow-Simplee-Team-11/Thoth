520cec5857f7e27725aec8b209848fc511e2fe05
sneaky-potato <ashwanikamal.im421@gmail.com>
2023-02-08 07:50:31 +0530

    Update start optimizer endpoint to POST

--- routes/service.route.js
+++ routes/service.route.js
@@ -3,7 +3,7 @@ import controllers from "../controllers/service.controller.js";
 
 const router = express.Router();
 
-router.get("/start", controllers.startOptimiser);
+router.post("/start", controllers.startOptimiser);
 router.post("/dynamic", controllers.startDynamicPickup);
 
 export default router;
